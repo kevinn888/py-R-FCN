93bbc98326721047eab6233752cfa92e47e86add
xineria <35769683+kevinn888@users.noreply.github.com>
2020-03-18 14:43:48 +0700

    Update proposal_layer.py

--- lib/rpn/proposal_layer.py
+++ lib/rpn/proposal_layer.py
@@ -1,15 +1,10 @@
-# --------------------------------------------------------
-# Faster R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick and Sean Bell
-# --------------------------------------------------------
-
 import caffe
 import numpy as np
 import yaml
+import sys
+sys.path.append('/content/py-R-FCN/lib')
 from fast_rcnn.config import cfg
-from generate_anchors import generate_anchors
+from rpn.generate_anchors import generate_anchors
 from fast_rcnn.bbox_transform import bbox_transform_inv, clip_boxes
 from fast_rcnn.nms_wrapper import nms
 
